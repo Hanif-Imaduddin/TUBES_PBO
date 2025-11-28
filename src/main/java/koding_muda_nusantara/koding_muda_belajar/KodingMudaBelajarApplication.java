8a0232d38ffedb9202b31db42355d751353b8250
Hanif Imaduddin <hanifimaduddin2@gmail.com>
2025-11-28 12:38:08 +0700

    Menambahkan file project

---
+++ src/main/java/koding_muda_nusantara/koding_muda_belajar/KodingMudaBelajarApplication.java
@@ -0,0 +1,13 @@
+package koding_muda_nusantara.koding_muda_belajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodingMudaBelajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodingMudaBelajarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koding_muda_nusantara/koding_muda_belajar/KodingMudaBelajarApplicationTests.java
@@ -0,0 +1,13 @@
+package koding_muda_nusantara.koding_muda_belajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodingMudaBelajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
